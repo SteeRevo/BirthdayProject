8a5dc0484722d4ebc0c4ed9cfd5dfb0ab6b33c75
SteeRevo <heyethan1@gmail.com>
2025-01-07 22:41:52 -0800

    started project

---
+++ Player1.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+
+const SPEED = 500.0
+
+
+
+func _physics_process(delta):
+
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_vector("Player1Left", "Player1Right", "Player1Up", "Player1Down")
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.y = direction.y * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+	
