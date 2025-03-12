9ab6765b3ef8ea9075be8b554b79428505a8ed6a
SteeRevo <heyethan1@gmail.com>
2025-03-12 00:24:44 -0700

    animations added

--- Player1_3D.gd
+++ Player1_3D.gd
@@ -5,6 +5,7 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 @onready var model = $MamaModel
 @onready var model_ap = $MamaModel/AnimationPlayer
+var fire = false
 
 
 func _physics_process(delta: float) -> void:
@@ -17,13 +18,29 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		if fire == true:
+			model_ap.play("Walk_Fire")
+			fire = false
+		else:
+			model_ap.queue("Walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		if fire == true:
+			model_ap.play("fire")
+		else:
+			model_ap.stop()
+			model_ap.queue("tpose")
 
 	move_and_slide()
-
+	model.rotation_degrees.y = $Shooter.rotation_degrees.y
 
 func _on_shooter_firing():
-	model_ap.play("fire")
-	model.rotation_degrees.y = $Shooter.rotation_degrees.y
+	fire = true
+	
+	
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "fire":
+		fire = false
